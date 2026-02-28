c0e668f771800f33be40b14863363af3b05aa408
Manuel Andres Mendieta <manuelmendieta21@gmail.com>
2026-02-28 19:40:45 +0100

    update favicon.png

---
+++ frontend/reportlabf/components/DataLabMobileLogo.jsx
@@ -0,0 +1,9 @@
+export function DataLabMobileLogo() {
+    return (
+        <img
+            src="/img/logoresolution.png"
+            alt="Logo DataLab"
+            className="w-45 dark:bg-white"
+        />
+    );
+}
